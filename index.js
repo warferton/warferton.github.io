edb75b888876b16956c06a3efd30dcca38a2738e
warferton <warferton45@gmail.com>
2021-05-01 18:18:53 +0500

    cleanup

--- index.js
+++
@@ -1,9 +0,0 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    lerp: 0.1,
-    multiplier: 1.1,
-    offset: 0
-});
